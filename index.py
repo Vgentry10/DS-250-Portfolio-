101cc600d694b75fa982a84d24f2c4055ad49a69
Vgentry10 <gen23003@byui.edu>
2026-07-18 20:56:32 -0700

    more unit 5 assignment edits and added a  new stretch assignment for task 4

--- index.py
+++ index.py
@@ -1,9 +1,8 @@
+
 # %%
 # Read in libraries
 import pandas as pd
 import numpy as np
 from lets_plot import *
 LetsPlot.setup_html(isolated_frame=True)
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-df = pd.read_csv("https://github.com/byuidatascience/data4dwellings/raw/master/data-raw/dwellings_ml/dwellings_ml.csv")
+url = 'https://github.com/fivethirtyeight/data/raw/master/star-wars-survey/StarWars.csv'
