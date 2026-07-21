da1d28434d3699230817702ed77f888e34c8213f
Vgentry10 <gen23003@byui.edu>
2026-07-20 18:58:42 -0700

    deleted index.py

--- index.py
+++
@@ -1,8 +0,0 @@
-
-# %%
-# Read in libraries
-import pandas as pd
-import numpy as np
-from lets_plot import *
-LetsPlot.setup_html(isolated_frame=True)
-url = 'https://github.com/fivethirtyeight/data/raw/master/star-wars-survey/StarWars.csv'
